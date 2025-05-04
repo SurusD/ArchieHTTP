c3974df4546d367726c860b6804db661c322cab4
Archie <surushlatipov792@gmail.com>
2025-05-04 20:47:01 +0400

    optimizations

--- archie_http.lua
+++ archie_http.lua
@@ -8,113 +8,105 @@ local function builder()
             if type(method) ~= "string" then
                 return error(invalidArgument("withMethod(self, method: string)", "string", type(method)))
             end
-            if string.lower(method) == "get" then
+            local method_lower = string.lower(method)
+            if method_lower == "get" then
                 self._method = false
-                return self
-            elseif string.lower(method) == "post" then
+            elseif method_lower == "post" then
                 self._method = true
-                return self
             else
                 return error(string.format(
-                    "Archie: invalid argmuent for function withMethod(self, method: string), GameGuardian only supports GET and POST requests not '%s'",
+                    "Archie: invalid argument for function withMethod(self, method: string), only GET and POST requests are supported, not '%s'",
                     method))
             end
+            return self
         end,
+
         withURL = function(self, url)
             if type(url) ~= "string" then
-                return error(invalidArgument("withURL(self, url: string", "string", type(url)))
+                return error(invalidArgument("withURL(self, url: string)", "string", type(url)))
             end
             self._url = url
             return self
         end,
+
         withHeaders = function(self, headers)
-            if type(headers) ~= "table" or type(headers) ~= "nil" then
-                return error(invalidArgument("withHeaders(self, headers: table | nil)", "table",
-                    type(headers)))
+            if type(headers) ~= "table" and headers ~= nil then
+                return error(invalidArgument("withHeaders(self, headers: table | nil)", "table | nil", type(headers)))
             end
             self._headers = headers
             return self
         end,
+
         withData = function(self, data)
             self._data = data
             return self
         end,
+
         onFail = function(self, callback)
             if type(callback) ~= "function" then
                 return error(invalidArgument("onFail(self, callback: function)", "function", type(callback)))
             end
-            if self._onFail then
-                local originallistener = self._onFail
-                self._onFail = function(response)
-                    originallistener(response)
-                    callback(response)
-                end
-            else
-                self._onFail = callback
-            end
+            self._onFail = self._onFail and function(response)
+                self._onFail(response)
+                callback(response)
+            end or callback
             return self
         end,
+
         onSuccess = function(self, callback)
             if type(callback) ~= "function" then
                 return error(invalidArgument("onSuccess(self, callback: function)", "function", type(callback)))
             end
-            if self._onSuccess then
-                local originalListener = self._onSuccess
-                self._onSuccess = function(response)
-                    originalListener(response)
-                    callback(response)
-                end
-            else
-                self._onSuccess = callback
-            end
+            self._onSuccess = self._onSuccess and function(response)
+                self._onSuccess(response)
+                callback(response)
+            end or callback
             return self
         end,
+
         autoload = function(self, withName)
             self:onSuccess(function(response)
+                local loadedContent = load(response.content)()
                 if withName then
-                    _G[tostring(withName)] = load(response.content)()
-                else
-                    load(response.content)()
+                    _G[tostring(withName)] = loadedContent
                 end
             end)
             return self
         end,
+
         request = function(self)
-            if not self._url then return error("Archie: request to what? Please provide URl: :withURL(url: string)") end
+            if not self._url then return error("Archie: request to what? Please provide URL using withURL(url: string)") end
             local url = self._url
-            local method = self._method or false
+            local method = self._method
+            local data = self._data
+            local headers = self._headers
+
             if method then
-                if self._data == nil then return error("Archie: cant request post method without providing data") end
-                local data = tostring(self._data)
-                local headers = self._headers
-                local response = gg.makeRequest(url, headers, data)
+                if data == nil then return error("Archie: can't request POST method without providing data") end
+                local response = gg.makeRequest(url, headers, tostring(data))
                 if type(response) == "table" and response.code == 200 then
                     self._onSuccess(response)
                 else
-                    self._onFail()
+                    self._onFail(response)
                 end
             else
-                local headers = self._headers
                 local response = gg.makeRequest(url, headers)
                 if type(response) == "table" and response.code == 200 then
-                    if self._onSuccess then
-                        self._onSuccess(response)
-                    end
+                    self._onSuccess(response)
                 else
-                    if self._onFail then
-                        self._onFail()
-                    end
+                    self._onFail(response)
                 end
             end
         end
-
     }
 end
+
 local lib = {
     make = function()
         return builder()
     end,
 }
+
 return setmetatable(lib, {
     __index = lib,
     __newindex = function(self, _, _)
