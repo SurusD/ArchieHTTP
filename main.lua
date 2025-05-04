6b1bbf2d1b74b04d02ecd684235d85a5ae93b935
Archie <surushlatipov792@gmail.com>
2025-05-04 19:05:38 +0400

    removed useless things

--- main.lua
+++ main.lua
@@ -7,37 +7,6 @@ if archie_http == nil then
     return error("failed to load archie http")
 end
 
-local function get(url, headers) -- headers can be nil
-    local response = nil
-    archie_http.make():
-        withURL(url):
-        withMethod("GET"):
-        withHeaders(headers):
-        onSuccess(function(resp)
-            response = resp
-        end):request()
-    return response
-end
-
-local function post(url, headers, data) -- headers can be nil but data not its required
-    local success = false
-    archie_http.make():
-        withData(data):
-        withURL(url):
-        withHeaders(headers):
-        withMethod("POST"):
-        onSuccess(function(_)
-            success = true
-        end):request()
-    return success
-end
-local google = get("https://google.com", nil)
-if google ~= nil then
-    print(google.code, " ", google.content)
-else
-    print("failed")
-end
--- or you can do
 archie_http.make():
     withMethod("GET"):
     withURL("https://google.com"):
@@ -48,3 +17,4 @@ archie_http.make():
         print(response.code, " ", response.content)
     end):
     request()
+-- for post request you need to change GET to POST and add :withData(data: string)
