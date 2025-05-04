ab6eb813bacd2e82f2322813c9f65de67075ab53
Archie <surushlatipov792@gmail.com>
2025-05-04 18:46:39 +0400

    added instalation from link

--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
-local http = require "archie_http"
+local archie_http = nil
+local response = gg.makeRequest("https://raw.githubusercontent.com/SurusD/ArchieHTTP/refs/heads/master/archie_http.lua")
+if type(response) == "table" and response.code == 200 then
+    archie_http = load(response.content, "lib", "t")
+end
+if archie_http == nil then
+    return error("failed to load archie http")
+end
 
 function get(url, headers) -- headers can be nil
     local response = nil
-    http.make():
+    archie_http.make():
         withURL(url):
         withMethod("GET"):
         withHeaders(headers):
@@ -14,7 +21,7 @@ end
 
 function post(url, headers, data) -- headers can be nil but data not its required
     local success = false
-    http.make():
+    archie_http.make():
         withData(data):
         withURL(url):
         withHeaders(headers):
